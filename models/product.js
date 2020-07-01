f4df735bad026f0254b5134b4dc46961cbf16fa8
Ngô Hảo <66462691+ngotrieuhao@users.noreply.github.com>
2020-07-01 16:44:33 +0700

    Add files via upload

---
+++ models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+var productSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    Name: String,
+    Price: String,
+    urlImg: String
+} , { collection: 'Products' } );
+
+var Product = mongoose.model('Products', productSchema);
+
+module.exports = Product;
